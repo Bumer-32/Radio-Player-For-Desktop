f79264ec629acfe161a0b91bc13cf3b5791ba3de
Artem Samchuk <krutog727@gmail.com>
2025-08-22 15:50:28 +0300

    Logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("application")
+    id("maven-publish")
 }
 
 group = "ua.pp.lumivoid"
@@ -11,6 +14,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
 
 kotlin {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 package ua.pp.lumivoid
 
+import org.slf4j.LoggerFactory
+
 fun main() {
+    Main.main()
+}
+
+object Main {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    fun main() {
+        logger.info("Starting!")
+    }
 }
