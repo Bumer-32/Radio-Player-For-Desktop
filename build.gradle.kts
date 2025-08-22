152783180a12669938c4ec2f1700dad9e489b810
Artem Samchuk <krutog727@gmail.com>
2025-08-22 17:10:50 +0300

    Hello world!

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,26 @@ repositories {
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("org.slf4j:jul-to-slf4j:2.0.17")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = "ua.pp.lumivoid.MainKt"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "ua.pp.lumivoid.MainKt"
+    }
+}
+
+javafx {
+    version = "21"
+    modules = listOf("javafx.controls")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,41 @@
 package ua.pp.lumivoid
 
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.image.Image
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
 import org.slf4j.LoggerFactory
+import org.slf4j.bridge.SLF4JBridgeHandler
+import kotlin.jvm.javaClass
 
 fun main() {
-    Main.main()
+    SLF4JBridgeHandler.removeHandlersForRootLogger()
+    SLF4JBridgeHandler.install()
+    Main().main()
 }
 
-object Main {
+class Main: Application() {
     private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    override fun start(stage: Stage) {
+        val javaVersion = System.getProperty("java.version")
+        val javafxVersion = System.getProperty("javafx.version")
+
+        logger.info("java version: $javaVersion")
+        logger.info("javafx version: $javafxVersion")
+
+        stage.icons.add(Image(javaClass.getResourceAsStream("rp_logo.png")))
+
+        val label = Label("JavaFX Version: $javafxVersion")
+        val scene = Scene(StackPane(label), 800.0, 600.0)
+        stage.scene = scene
+        stage.show()
+    }
 
     fun main() {
         logger.info("Starting!")
+        launch()
     }
 }
