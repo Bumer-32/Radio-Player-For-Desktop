67807aaa796a08fac58afc3e0ca7c0a01f15983b
Artem Samchuk <79054301+Bumer-32@users.noreply.github.com>
2025-11-01 18:06:27 +0200

    fuck mvvm

--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RadioPlayerForDesktop.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace RadioPlayerForDesktop.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
